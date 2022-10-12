bda94ced152be93217c421735af8125884047777
unknown <meiltokalicharan@gmail.com>
2022-10-12 20:16:06 +0530

    to create a push caljs range user

---
+++ cal.js
@@ -0,0 +1,7 @@
+var c = document.getElementById("chemistry").value;
+  
+  var p = document.getElementById("physics").value;
+  var m = document.getElementById("maths").value;
+  var com = document.getElementById("computerscience").value;
+  
+  document.getElementById("demo").innerHTML = c + p + m + com;
